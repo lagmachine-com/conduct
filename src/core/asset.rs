8a2ea4225b13ea33e37e14629c352fe000c26f98
Airyzz <36567925+Airyzz@users.noreply.github.com>
2024-10-22 18:38:47 +1030

    Adjust formatting and parsing of asset data

--- src/core/asset.rs
+++ src/core/asset.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 use log::{debug, info};
 use serde::{Deserialize, Serialize};
+use serde_yaml::Value;
 
 pub enum AssetEntry {
     Asset(Asset),
@@ -21,58 +22,81 @@ pub struct AssetCategory {
     pub children: HashMap<String, AssetEntry>,
 }
 
-fn is_mapping_asset(map: &serde_yaml::Mapping) -> bool {
-    return map.contains_key("departments");
-}
-
-pub fn parse_entry_as_asset(map: &serde_yaml::Mapping, key: String) -> Asset {
-    let mut data = map.clone();
-    data.insert(
-        serde_yaml::Value::String("name".to_string()),
-        serde_yaml::Value::String(key),
-    );
-
-    let asset = serde_yaml::from_value::<Asset>(serde_yaml::Value::Mapping(data))
-        .expect("Unable to parse asset");
-
-    debug!("\tReading asset: {}", asset.name);
-
-    asset
-}
-
-pub fn parse_entry(map: &serde_yaml::Mapping, key: String) -> AssetEntry {
-    if is_mapping_asset(map) {
-        return AssetEntry::Asset(parse_entry_as_asset(map, key));
-    }
-
-    debug!("Reading asset category: {}", key);
-
-    let mut category = AssetCategory {
-        name: key.to_owned(),
+pub fn parse_category_assets(value: &Vec<serde_yaml::Value>, key: String) -> AssetEntry {
+    let mut result = AssetCategory {
+        name: key,
         children: HashMap::new(),
     };
 
-    for key in map.keys() {
-        let value = map.get(key).unwrap();
+    for asset_entry in value.iter() {
+        let mapping = asset_entry
+            .as_mapping()
+            .expect("Unable to read asset as mapping");
 
-        if !value.is_mapping() {
-            debug!(
-                "Invalid value found in assets at key '{}'",
-                key.as_str().unwrap()
-            );
-        }
+        let key = mapping
+            .keys()
+            .next()
+            .expect("Unable to get a key for the asset entry")
+            .as_str()
+            .expect("Asset key was not a valid string");
 
-        let key = key.as_str().expect("Asset key was not a valid string");
-        let map = value.as_mapping().expect("Asset was not a valid mapping");
-        let entry = parse_entry(map, key.to_string());
-        category.children.insert(key.to_string(), entry);
+        debug!("Found asset key: {}", key);
+
+        let mut data = mapping.clone();
+        data.insert(
+            Value::String("name".to_string()),
+            Value::String(key.to_string()),
+        );
+
+        let asset = serde_yaml::from_value::<Asset>(Value::Mapping(data))
+            .expect("Unable to parse asset form yaml data");
+
+        result
+            .children
+            .insert(key.to_string(), AssetEntry::Asset(asset));
     }
 
-    return AssetEntry::Category(category);
+    return AssetEntry::Category(result);
+}
+
+pub fn parse_category(value: &serde_yaml::Mapping, key: String) -> AssetEntry {
+    let mut result = AssetCategory {
+        name: key.clone(),
+        children: HashMap::new(),
+    };
+
+    debug!("Parsing asset category: {}", key);
+
+    for entry in value.iter() {
+        let key = entry
+            .0
+            .as_str()
+            .expect("Asset category key was not valid string")
+            .to_string()
+            .clone();
+
+        let data = entry.1;
+
+        result.children.insert(key.clone(), parse_entry(data, key));
+    }
+
+    return AssetEntry::Category(result);
+}
+
+pub fn parse_entry(value: &serde_yaml::Value, key: String) -> AssetEntry {
+    let result = match value {
+        Value::Sequence(vec) => parse_category_assets(vec, key),
+        Value::Mapping(mapping) => parse_category(mapping, key),
+        _ => panic!("Invalid entry in asset entries"),
+    };
+
+    result
 }
 
 pub fn from_yaml(map: &serde_yaml::Mapping) -> AssetCategory {
-    let entry = parse_entry(map, "assets".to_string());
+    let value = serde_yaml::Value::Mapping(map.clone());
+
+    let entry = parse_entry(&value, "assets".to_string());
 
     match entry {
         AssetEntry::Asset(_asset) => {
