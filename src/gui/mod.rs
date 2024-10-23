a55781fc3507678cdfc6669b057069c4a09855fc
Airyzz <36567925+Airyzz@users.noreply.github.com>
2024-10-23 23:57:17 +1030

    Experimenting with wry and custom protocol

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,27 +1,82 @@
-mod message;
-mod update;
-mod view;
+use std::borrow::Cow;
 
-use iced::{Task, Theme};
+use include_directory::include_directory;
+use log::info;
+use tao::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+use wry::{http::Response, WebViewBuilder};
 
-struct UIState {
-    project: crate::core::project::Project,
-    value: i32,
-}
-
-impl UIState {
-    pub fn theme(&self) -> Theme {
-        Theme::Ferra
-    }
-}
+static PROJECT_DIR: include_directory::Dir = include_directory!("$CARGO_MANIFEST_DIR/ui/dist");
 
 pub fn gui(project: crate::core::project::Project) {
-    let state = UIState {
-        project: project,
-        value: 0,
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("conduct")
+        .build(&event_loop)
+        .unwrap();
+
+    let builder = WebViewBuilder::new()
+        .with_url("conduct://home")
+        .with_custom_protocol("conduct".to_string(), |a, b| {
+            let mut path = b.uri().path();
+            if path.eq("/") {
+                path = "index.html"
+            }
+            info!("Requesting file: {}", path);
+
+            for entry in PROJECT_DIR.entries().iter() {
+                info!("Has entry: {}", entry.path().to_str().unwrap());
+            }
+
+            if PROJECT_DIR.contains(path) {
+                let data = PROJECT_DIR.get_file(path);
+                let str = data.unwrap().contents_utf8().unwrap();
+                Response::builder()
+                    .status(200)
+                    .header("Content-Type", "text/html; charset=utf-8")
+                    .body(Cow::Owned(str.into()))
+                    .unwrap()
+            } else {
+                Response::builder()
+                    .status(404)
+                    .header("Content-Type", "text/html; charset=utf-8")
+                    .body(Cow::Owned("Not found".into()))
+                    .unwrap()
+            }
+        });
+
+    #[cfg(any(
+        target_os = "windows",
+        target_os = "macos",
+        target_os = "ios",
+        target_os = "android"
+    ))]
+    let _webview = builder.build(&window)?;
+    #[cfg(not(any(
+        target_os = "windows",
+        target_os = "macos",
+        target_os = "ios",
+        target_os = "android"
+    )))]
+    let _webview = {
+        use tao::platform::unix::WindowExtUnix;
+        use wry::WebViewBuilderExtUnix;
+        let vbox = window.default_vbox().unwrap();
+        builder.build_gtk(vbox).unwrap()
     };
 
-    _ = iced::application("Conduct", UIState::update, UIState::view)
-        .theme(UIState::theme)
-        .run_with(|| (state, Task::none()));
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        if let Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } = event
+        {
+            *control_flow = ControlFlow::Exit;
+        }
+    });
 }
