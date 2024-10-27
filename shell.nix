b644bbe585025c73058184c2c285dd78614dbeb3
Airyzz <36567925+Airyzz@users.noreply.github.com>
2024-10-27 18:34:55 +1030

    add nix shell for development

---
+++ shell.nix
@@ -0,0 +1,23 @@
+
+let unstableTarball = fetchTarball https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
+pkgs = import <nixpkgs> {};
+unstable = import unstableTarball {};
+
+shell = pkgs.mkShell {
+  buildInputs = with pkgs; [
+    unstable.rustc
+    unstable.cargo
+    unstable.rustfmt
+    unstable.rust-analyzer
+    unstable.clippy
+    nodejs
+    webkitgtk_4_1
+    pkg-config
+    libsoup_3
+  ];
+
+  RUST_BACKTRACE = 1;
+  WEBKIT_DISABLE_COMPOSITING_MODE=1;
+};
+
+in shell
