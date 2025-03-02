ae69ffbc7d48f07812f9a2e406bf992f5475a076
Airyzz <36567925+Airyzz@users.noreply.github.com>
2025-03-02 12:07:38 +1030

    Update resolved_element_data.rs

--- src/core/element/resolved_element_data.rs
+++ src/core/element/resolved_element_data.rs
@@ -27,7 +27,7 @@ impl ResolvedElementData {
     }
 
     pub fn set_shot(&mut self, value: &String) {
-        self.shot = Some(value);
+        self.shot = Some(value.clone());
     }
 
     pub fn get_shot(&self) -> Option<String> {
